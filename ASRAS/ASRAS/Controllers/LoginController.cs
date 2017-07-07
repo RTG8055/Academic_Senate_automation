cdbca77566c19b49d93e4a6bdd0d21e93b56f225
BikramjeetSingh <singhbikramjeet@outlook.com>
2017-07-07 17:17:45 +0530

    Initial

---
+++ ASRAS/ASRAS/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASRAS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Clicked(string username, string password)
+        {
+            if (username.Equals("robby") && password.Equals("singh"))
+            {
+                ViewBag.Result = "Right";
+            }
+            else
+            {
+                ViewBag.Result = "Wrong";
+            }
+
+
+            return View("Login");
+        }
+    }
+}
---
+++ ASRAS/ASRAS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASRAS.MvcApplication" Language="C#" %>
