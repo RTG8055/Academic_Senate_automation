8e0783adb5850b48341d4b1730f0b717d736d880
RTG <RTG@DESKTOP-HSRRFBA>
2017-07-28 22:07:51 +0530

    added new subject partially

--- ASRAS/ASRAS/Controllers/ChangeInCurriculumController.cs
+++ ASRAS/ASRAS/Controllers/ChangeInCurriculumController.cs
@@ -132,7 +132,12 @@ namespace ASRAS.Controllers
             ViewBag.Uname = TempData["UserName2"];
             return View();
         }
-
+        public ActionResult newSemesterView()
+        {
+            TempData["UserName4"] = TempData["UserName3"];
+            ViewBag.Uname = TempData["UserName3"];
+            return View("newSemesterView");
+        }
         //public static InstituteViewModal ivm = new InstituteViewModal(); 
         //public ActionResult InstituteView()
         //{
