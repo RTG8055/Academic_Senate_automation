1edab8c3e00bbf85831da10866910937e1fa2915
RTG <RTG@DESKTOP-HSRRFBA>
2017-07-28 22:10:36 +0530

    ReAllocate

--- ASRAS/ASRAS/Controllers/ChangeInCurriculumController.cs
+++ ASRAS/ASRAS/Controllers/ChangeInCurriculumController.cs
@@ -132,6 +132,12 @@ namespace ASRAS.Controllers
             ViewBag.Uname = TempData["UserName2"];
             return View();
         }
+        public ActionResult ReAllocate()
+        {
+            TempData["UserName4"] = TempData["UserName3"];
+            ViewBag.Uname = TempData["UserName3"];
+            return View("ReAllocate");
+        }
         public ActionResult newSemesterView()
         {
             TempData["UserName4"] = TempData["UserName3"];
