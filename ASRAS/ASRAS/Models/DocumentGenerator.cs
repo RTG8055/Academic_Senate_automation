2f8e33264302cf9834927ef7ea7a74ada2df474d
BikramjeetSingh <singhbikramjeet@outlook.com>
2017-07-29 16:26:05 +0530

    Added CR and CS pages

--- ASRAS/ASRAS/Models/DocumentGenerator.cs
+++ ASRAS/ASRAS/Models/DocumentGenerator.cs
@@ -9,133 +9,23 @@ namespace ASRAS.Models
 {
     public static class DocumentGenerator
     {
-        public static Word.Document insertAbstract(Word.Document document, string abstractContent)
+        public static Word.Document insert(Word.Document document, string heading, string content)
         {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = "ABSTRACT";
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
+            Word.Paragraph headingPara;
+            headingPara = document.Content.Paragraphs.Add();
+            headingPara.Range.Text = heading.ToUpper();
+            headingPara.Range.Font.Bold = 1;
+            headingPara.Range.Font.Size = 14;
+            headingPara.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            headingPara.Range.InsertParagraphAfter();
 
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = abstractContent;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document insertObjectives(Word.Document document, string objectivesContent)
-        {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = "OBJECTIVES";
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
-
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = objectivesContent;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document insertOutcomes(Word.Document document, string outcomesContent)
-        {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = "OUTCOMES";
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
-
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = outcomesContent;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document insertMain(Word.Document document, string mainContent)
-        {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = "MAIN CONTENT";
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
-
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = mainContent;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document insertReferences(Word.Document document, string referencesContent)
-        {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = "REFERENCES";
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
-
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = referencesContent;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document inserCustomTitle(Word.Document document, string title)
-        {
-            Word.Paragraph heading;
-            heading = document.Content.Paragraphs.Add();
-            heading.Range.Text = title.ToUpper();
-            heading.Range.Font.Bold = 1;
-            heading.Range.Font.Size = 14;
-            heading.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
-            heading.Range.InsertParagraphAfter();
-
-            return document;
-        }
-
-        public static Word.Document insertCustomeText(Word.Document document, string text)
-        {
-            Word.Paragraph content;
-            content = document.Content.Paragraphs.Add();
-            content.Range.Text = text;
-            content.Range.Font.Bold = 0;
-            content.Range.Font.Size = 11;
-            content.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
-            content.Range.InsertParagraphAfter();
+            Word.Paragraph contentPara;
+            contentPara = document.Content.Paragraphs.Add();
+            contentPara.Range.Text = content;
+            contentPara.Range.Font.Bold = 0;
+            contentPara.Range.Font.Size = 11;
+            contentPara.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            contentPara.Range.InsertParagraphAfter();
 
             return document;
         }
